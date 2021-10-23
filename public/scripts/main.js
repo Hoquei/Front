8f8787bab760318d0ddc0587bf0ae8a66f57e72f
Gabriel <gabrielmacielboituva19@gmail.com>
2021-10-23 14:24:04 -0300

    Movimentação Diagonal Player 1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,6 +8,8 @@ let Nameplayer1 = 'player 1';
 let Nameplayer2 = 'player 2';
 let counter = 3
 
+
+
 var Player = {
 	new: function (side, numHitter) {
 		return {
@@ -25,6 +27,46 @@ var Player = {
 		};
 	},
 }
+var Game = {
+    player1move: function(direction) {
+        console.log(typeof(direction));
+        
+        if (direction > 350 || direction < 10){
+            moveBaixo = false; moveCima = false; moveEsquerda = false; moveDireita = true;
+        } else if (direction < 100 && direction > 80){
+            moveBaixo = false; moveCima = true; moveEsquerda = false; moveDireita = false;
+        } else if (direction < 190 && direction > 170){
+            moveBaixo = false; moveCima = false; moveEsquerda = true; moveDireita = false;
+        }  else if (direction < 280 && direction > 250){
+            moveBaixo = true; moveCima = false; moveEsquerda = false; moveDireita = false;
+        }
+        
+        else if (direction > 10 && direction < 80 ){
+            moveBaixo = false; moveCima = true; moveEsquerda = false; moveDireita = true;
+        } else if (direction > 100 && direction < 170){
+        moveBaixo = false; moveCima = true; moveEsquerda = true; moveDireita = false;
+        } else if (direction > 190 && direction < 250){
+            moveBaixo = true; moveCima = false; moveEsquerda = true; moveDireita = false;
+        }  else if (direction > 280 && direction < 350){
+            moveBaixo = true; moveCima = false; moveEsquerda = false; moveDireita = true;
+        }
+
+        render();
+    },
+    player1ChangeDirection: function(direction){
+       console.log(direction);
+        
+    },
+    player2move: function(direction) {
+        console.log(direction);
+    },
+
+    //Player stop moving function.
+    player1stop: function() {
+        moveBaixo = moveCima = moveEsquerda = moveDireita = false;
+    }
+}
+
 
 textModal('Bem vindo!', 'Esperando players', 'Para jogar conecte no /controller');
 
@@ -48,33 +90,26 @@ socket.on("player_join", (nickName) => {
         console.log('limite de players atingido');
     } 
 })
-box = document.getElementById('test');
 
-// TEST
-var cnv = document.querySelector("canvas");
-var ctx = cnv.getContext("2d");
-ctx.fillRect(10,20,50,80);
+//Player move
+socket.on('player1move', (direction) => {
+    console.log(direction);
+    Game.player1move(direction);
+})
+socket.on('player2move', (direction) => {
+    console.log(direction);
+    Game.player2move(direction);
+})
 
-var Game = {
-    initialize: function() {
-        this.player1 = Player.new.call(this, 'left', 1);
-        var speed = 10
-        socket.on('player1move', (direction) => {
-            
-            switch(direction){
-                case 'right':
-                    
-                    break;
-            }
-            console.log(direction);
-        })
-        
-        socket.on('player2move', (direction) => {
-            console.log(direction);
-        })
-    }
-}
-
+ //Player stop moving
+socket.on('player1stop', () => {
+    console.log("stopped");
+    Game.player1stop();
+})
+socket.on('player2stop', (direction) => {
+    console.log(direction);
+    Game.player2move(direction);
+})
 
 function textModal(bemVindo, waiting, play, player1) {
     bemVindo ? document.getElementById("BemVindo").innerHTML = bemVindo : document.getElementById("BemVindo").innerHTML = '';
@@ -106,3 +141,37 @@ function timer() {
     }, 1000);
 }
 
+// TEST
+
+var cnv = document.querySelector("canvas");
+var ctx = cnv.getContext("2d");
+ctx.fillRect(10,20,50,80);
+
+var widght = cnv.width;
+var height = cnv.height;
+var UP = 38, DOWN = 40, LEFT = 37, RIGHT = 39;
+var moveEsquerda = false, moveDireita = false, moveCima = false, moveBaixo = false;
+
+var p1 = {
+    x: 10,
+    y: 10
+};
+
+update();
+function move(){
+    if(moveEsquerda && p1.x > 0) p1.x--;
+    if(moveDireita && p1.x < widght-100) p1.x++;
+    if(moveCima && p1.y > 0) p1.y--;
+    if(moveBaixo && p1.y < height-100) p1.y++;
+}
+
+function render(){
+    ctx.clearRect(0,0,cnv.width,cnv.height);
+    ctx.fillRect(p1.x, p1.y, 50, 50)
+}
+
+function update(){
+    requestAnimationFrame(update, cnv);
+    move();
+    render();
+}
